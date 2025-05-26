4c97f0b06e8e9b347801b6c3713287d34eb32402
Mark Glenn M. Gabuya <gabuya.markglenn@gmail.com>
2025-05-26 12:09:51 +0800

    feature/quotes/sample-data: Resuing the fixture files to create fake data for testing the development database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# Reusing the data from fixtures files in test environment unto the development environment.
+# Meaning that fake data from test will be reuse in development environment.
+puts "\n== Seeding the databse with fixtures"
+system("bin/rails db:fixtures:load")
+
+# Running the % bin/rails db:seed is now equivalent to removing all the quotes and loading fixtures as development data. 
+# Always run the % bin/rails db:seed command to generate fake data. 
