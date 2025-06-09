d21754eff97eb466aaea2dcff955c7b3cd900efd
Mark Glenn M. Gabuya <gabuya.markglenn@gmail.com>
2025-06-09 16:51:22 +0800

    Chapter 5: Creating and broadcasting the created quotes in realtime with turbo streams. Utilize the redis and update it so that we can test the turbo streams in the console. As well as applying syntactic sugar to the code we wrote for creating quote and broadcasting it from the index.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,4 +5,8 @@ class Quote < ApplicationRecord
 
   # Adds a scope to order the list of Quotes from the newest first
   scope :ordered, -> { order(id: :desc) }
+
+  # Broadcast the HTML of the created quote to our users right after it was created
+  #after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_create_commit -> { broadcast_prepend_to "quotes" } # => This is the syntactic sugar equivalent of the code above
 end
