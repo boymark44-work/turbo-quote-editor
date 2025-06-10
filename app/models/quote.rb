f6a798340a19d02a720d7accf77c3bcd19bc4224
Mark Glenn M. Gabuya <gabuya.markglenn@gmail.com>
2025-06-10 13:23:39 +0800

    Chapter 5: Applying syntactic sugar that still implements quote creation, update, and deletion to the quotes stream asynchronously.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -6,13 +6,12 @@ class Quote < ApplicationRecord
   # Adds a scope to order the list of Quotes from the newest first
   scope :ordered, -> { order(id: :desc) }
 
-  # Broadcast the HTML of the created quote to our users right after it was created
-  #after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
-  after_create_commit -> { broadcast_prepend_later_to "quotes" } # => This is the syntactic sugar equivalent of the code above
 
-  # Broadcast quote updates
-  after_update_commit -> { broadcast_replace_later_to "quotes" }
+  #after_create_commit -> { broadcast_prepend_later_to "quotes" } 
+  #after_update_commit -> { broadcast_replace_later_to "quotes" }
+  #after_destroy_commit -> { broadcast_remove_to "quotes" }
 
-  # Broadcast quote deletes
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # This single-line is equivalent to broadcast create, update, and destroy which is the code above. 
+  # Applying this syntactic sugar means that we want to broadcast quote creation, updates, and deletions to the "quotes" stream asynchronously.
+  broadcasts_to -> (quote) { "quotes" }, inserts_by: :prepend
 end
